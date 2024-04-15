fe45b947da91e925c217b478e6602e3f5eb4d73a
Darma007 <118422177+Darma007@users.noreply.github.com>
2024-04-15 20:58:21 +0530

    Delete restaraunt_management directory

--- restaraunt_management/src/main/java/com/example/restaraunt_management/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.restaraunt_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.restaraunt_management.model.User;
-
-public interface UserRepo extends JpaRepository<User,Integer>{
-    
-}
